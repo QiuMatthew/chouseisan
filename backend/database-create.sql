5a7a6003a0e9a9c5b34cc073c0a81b98291e21a7
QiuMatthew <40983438+QiuMatthew@users.noreply.github.com>
2024-01-04 01:47:58 +0900

    Update database-create.sql

--- backend/database-create.sql
+++ backend/database-create.sql
@@ -4,58 +4,38 @@ USE chouseisan;
 -- created chouseisan database and use it.
 
 
-CREATE TABLE event (
-  event_id   VARBINARY(16) PRIMARY KEY,
+CREATE TABLE events (
+  event_id   VARCHAR(255) PRIMARY KEY,
   title      VARCHAR(128) NOT NULL,
   detail     TEXT,
-  creatorToken  VARBINARY(16) NOT NULL
+  host_token  VARCHAR(255) NOT NULL
 );
 
--- CREATE TABLE user (
---     user_id VARBINARY(16) PRIMARY KEY,
---     user_email VARCHAR(255),
---     recent_event1 VARBINARY(16),
---     recent_event2 VARBINARY(16),
---     recent_event3 VARBINARY(16),
---     recent_event4 VARBINARY(16),
---     recent_event5 VARBINARY(16),
---     FOREIGN KEY (recent_event1) REFERENCES event(event_id),
---     FOREIGN KEY (recent_event2) REFERENCES event(event_id),
---     FOREIGN KEY (recent_event3) REFERENCES event(event_id),
---     FOREIGN KEY (recent_event4) REFERENCES event(event_id),
---     FOREIGN KEY (recent_event5) REFERENCES event(event_id)
--- );
-
-CREATE TABLE event_user (
+CREATE TABLE event_users (
   id INT PRIMARY KEY AUTO_INCREMENT,
   event_id VARBINARY(16),
-  user_id VARBINARY(16),
-  FOREIGN KEY (event_id) REFERENCES event(event_id),
-  FOREIGN KEY (user_id) REFERENCES user(user_id)
+  user_name VARCHAR(255),
+  FOREIGN KEY (event_id) REFERENCES events(event_id)
 ) ENGINE MEMORY;
 
-CREATE TABLE event_timeslot (
-  id INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE event_timeslots (
+  event_timeslot_id INT PRIMARY KEY AUTO_INCREMENT,
   event_id VARBINARY(16),
-  timeslot_id INT,
   description VARCHAR(255),
-  FOREIGN KEY (event_id) REFERENCES event(event_id)
+  FOREIGN KEY (event_id) REFERENCES events(event_id)
 ) ENGINE MEMORY;
 
-CREATE TABLE event_user_timeslot (
+CREATE TABLE event_user_timeslots (
   id INT PRIMARY KEY AUTO_INCREMENT,
   event_id VARBINARY(16),
   user_id VARBINARY(16),
   timeslot_id INT,
-  Preference INT,
+  preference INT,
   user_name VARCHAR(128),
-  FOREIGN KEY (event_id) REFERENCES event(event_id),
-  FOREIGN KEY (user_id) REFERENCES user(user_id),
-  FOREIGN KEY (timeslot_id) REFERENCES event_timeslot(timeslot_id)
+  FOREIGN KEY (event_id) REFERENCES events(event_id),
+  FOREIGN KEY (timeslot_id) REFERENCES event_timeslots(event_timeslot_id)
 ) ENGINE MEMORY;
 
-CREATE INDEX idx_event_id USING HASH ON event(event_id);
-CREATE INDEX idx_event_id USING HASH ON event_user(event_id);
-CREATE INDEX idx_event_id USING HASH ON event_user_timeslot(event_id);
-
-INSERT INTO event VALUES (UUID_TO_BIN(UUID()), 'first event', 'description')
+CREATE INDEX idx_event_id USING HASH ON events(event_id);
+CREATE INDEX idx_event_id USING HASH ON event_users(event_id);
+CREATE INDEX idx_event_id USING HASH ON event_user_timeslots(event_id);
