39d7f497d9d4ebd4bb437f0c8f55e449b76e85f9
QiuMatthew <40983438+QiuMatthew@users.noreply.github.com>
2024-01-04 01:47:24 +0900

    Update events.go

--- backend/events/events.go
+++ backend/events/events.go
@@ -1,103 +1,169 @@
 package events
 
 import (
-	"fmt"
-	"log"
-	"net/http"
-	"time"
-
 	"github.com/gin-gonic/gin"
+	"net/http"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"log"
+	"github.com/google/uuid"
 )
 
-var db *gorm.DB
-
-type Event struct {
-	ID               uint   `gorm:"primarykey"`
+type RequestInfo struct {
+	EventID			 string   `json:"eventID"`
 	Title            string `json:"title"`
-	HostName         string `json:"hostName"`
 	DateTimeProposal string `json:"dateTimeProposal"`
 	Detail           string `json:"detail"`
-	//Hash 		   	 string `json:"hash"`
+	UserID			 uint	`json:"userID"`
+	UserName		 string	`json:"userName"`
+	UserEmail		 string	`json:"userEmail"`
+	UserToken		 string	`json:"userToken"`
+	HostID			 uint	`json:"hostID"`
+	HostName		 string	`json:"hostName"`
+	HostEmail		 string	`json:"hostEmail"`
+	HostToken		 string	`json:"hostToken"`
 }
 
-var events = make(map[string]Event)
+type Event struct {
+	EventID			 string   `gorm:"primarykey"`
+	Title            string `json:"title" gorm:"column:title"`
+	Detail           string `json:"detail" gorm:"column:detail"`
+	HostToken        	 string `gorm:"column:host_token"`
+}
+
+type EventUser struct {
+	ID				uint	`gorm:"primarykey"`
+	EventID			uint	`gorm:"column:event_id"`
+	UserName			string	`gorm:"column:user_name"`
+}
+
+type EventTimeslot struct {
+	EventTimeslotID	 uint	`gorm:"primarykey"`
+	EventID			 uint	`gorm:"column:event_id"`
+	Description		 string	`gorm:"column:description"`
+}
+
+// Declare db as global variable, not optimal but useful at the moment
+var db *gorm.DB
 
 func init() {
-	var err error
-	// Connect to mysql database
+	// This function will run immediately after backend is started, used to initialize database connection
 
-	time.Sleep(6 * time.Second)
-	db, err := gorm.Open("mysql", "root:mysql@tcp(mysql:3306)/chouseisan?charset=utf8&parseTime=True&loc=Local")
+	var err error
+
+	// Connect to mysql database
+    DBMS := "mysql"
+    USER := "root"
+    PASS := ""
+    PROTOCOL := "tcp(localhost:3306)"
+    DBNAME := "chouseisan"
+    CONNECT := USER + ":" + PASS + "@" + PROTOCOL + "/" + DBNAME + "?charset=utf8&parseTime=True&loc=Local"
+	db, err = gorm.Open(DBMS, CONNECT)
 	if err != nil {
-		fmt.Println("Failed to connect to database", err)
 		log.Fatal(err)
 		panic("failed to connect database")
 	}
-
+	
+	// check if database connection is correctly established 
 	if db == nil {
-		log.Fatal("db nil")
+        log.Println("ERROR: db is nil!")
 	}
-
+	
 	// Migrate the schema
 	db.AutoMigrate(&Event{})
 }
 
-func GetEventByHash(c *gin.Context) {
-	hash := c.Query("hash")
-	if hash == "" {
-		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "hash parameter is missing"})
-		return
-	}
-
-	event, ok := events[hash]
-	if !ok {
-		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "event not found"})
-		return
-	}
-	c.IndentedJSON(http.StatusOK, event)
-}
-
 func GetEvents(c *gin.Context) {
+    // GET /event
+	var events []Event
+	if err := db.Find(&events).Error; err != nil {
+		log.Fatal(err)
+		return
+	}
+	
 	c.IndentedJSON(http.StatusOK, events)
-	/*
-		var event Event
-		if err := db.Find(&event).Error; err != nil {
-			c.IndentedJSON(http.StatusInternalServerError, gin.H{"message": "failed to get events"})
-			return
-		}
-		c.IndentedJSON(http.StatusOK, events)
-	*/
 }
 
 func CreateEvent(c *gin.Context) {
-	var newEvent Event
+    // POST /event
+	var requestInfo RequestInfo
 
-	if err := c.BindJSON(&newEvent); err != nil {
+	if err := c.BindJSON(&requestInfo); err != nil {
 		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "invalid request body"})
 		return
 	}
 
-	// add to memory
-	events[newEvent.Title] = newEvent
-
-	// print out the event
-	log.Println(newEvent)
-
-	newEvent = Event{
-		Title:            "Sample Event",
-		HostName:         "John Doe",
-		DateTimeProposal: "2023-12-31 18:00:00",
-		Detail:           "A sample event description",
+	token := requestInfo.HostToken
+	if token == "" {
+		// new user
+		// generate a token for the creator
+		token = uuid.New().String()
+		log.Println("New token created:", token)
 	}
 
-	// insert into database
+	// insert event info into events table
+	newEventID := uuid.New().String()
+	newEvent := Event {
+		EventID:			newEventID,
+		Title:				requestInfo.Title,
+		Detail:				requestInfo.Detail,
+		HostToken:			token,
+	}
+
 	if err := db.Create(&newEvent).Error; err != nil {
 		c.IndentedJSON(http.StatusInternalServerError, gin.H{"message": "failed to create event"})
-		log.Println(err)
+		log.Println("Gorm Error:", err)
+		return
+	}
+
+	// get event id from database
+	var eventID uint
+	if err := db.Model(&Event{}).Select("event_id").Where("title = ?", requestInfo.Title).Row().Scan(&eventID); err != nil {
+		return
+	}
+	
+	// insert event user info into event_users table
+	newEventUser := EventUser {
+		ID:					0,	// auto increment
+		EventID:			eventID,
+		UserName:			requestInfo.UserName,
+	}
+	
+	if err := db.Create(&newEventUser).Error; err != nil {
+		c.IndentedJSON(http.StatusInternalServerError, gin.H{"message": "failed to create event user entry"})
+		log.Println("Gorm Error:", err)
 		return
 	}
 
 	c.IndentedJSON(http.StatusCreated, newEvent)
+}
+
+func DeleteEvent(c *gin.Context) {
+    // DELETE /event/
+
+	var requestInfo RequestInfo
+
+	if err := c.BindJSON(&requestInfo); err != nil {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "invalid request body"})
+		return
+	}
+
+	// get host token of the event
+    var hostToken string
+	if err := db.Model(&Event{}).Select("host_token").Where("event_id = ?", requestInfo.EventID).Row().Scan(&hostToken); err != nil {
+		return
+	}
+	
+	// check if the user is the host of the event
+    if hostToken == requestInfo.HostToken {
+        // delete event from database
+		if err := db.Where("event_id = ?", requestInfo.EventID).Delete(&Event{}).Error; err != nil {
+			c.IndentedJSON(http.StatusInternalServerError, gin.H{"message": "failed to delete event"})
+			log.Println("Gorm Error:", err)
+			return
+		}
+    } else {
+        c.IndentedJSON(http.StatusForbidden, gin.H{"message": "permission denied, you are not the host of the event"})
+        return
+    }
 }
